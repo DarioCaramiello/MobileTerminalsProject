96094e5a4c752117c8a0c93a9606a730b9f460d0
dario <dariocaramiello03@hotmail.com>
2023-02-15 02:08:30 +0100

    Start MobileTerminalsProject

--- app/src/main/java/com/example/mobileterminalsproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileterminalsproject/MainActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
